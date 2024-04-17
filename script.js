4de736cad6de622af9cce9abde8092bb85734f81
Kayque Ferreira <10425657@mackenzista.com.br>
2024-04-16 22:43:46 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleMode() {
+    const html = document.documentElement
+
+//     if (html.classList.contains("light")) {
+//         html.classList.remove("light")
+//     } else {
+//         html.classlist.add("light")
+//     }
+
+    html.classList.toggle("light")
+
+ }
